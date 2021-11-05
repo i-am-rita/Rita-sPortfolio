a7fa1448f35ba94fc557cfd92c9c9470a93cc630
i-am-rita <ritauzoma07@gmail.com>
2021-11-06 00:57:27 +0100

    updated about page and added hamburger

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const toggle = document.querySelector('.toggle')
-const navigation = document.querySelector('.navigation')
+// const toggle = document.querySelector('.toggle')
+// const navigation = document.querySelector('.navigation')
 
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active')
-  navigation.classList.toggle('active')
-})
+// toggle.addEventListener('click', () => {
+//   toggle.classList.toggle('active')
+//   navigation.classList.toggle('active')
+// })
+
+// function toggle(){
+//   let menuOpen = document.querySelector('.toggler').checked;
+// if(menuOpen = true){
+//           document.querySelector('.toggler').checked = false;
+//       }
+// }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
